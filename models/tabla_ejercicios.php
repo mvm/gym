50bd2d59bc6c7e3120e267e5a56c7a7c04f563df
Miguel Vicente Moure <mvicmoure@gmail.com>
2017-01-05 10:55:24 +0100

    Corregido tabla_ejercicios.php

--- models/tabla_ejercicios.php
+++ models/tabla_ejercicios.php
@@ -3,30 +3,30 @@ class TablaEjercicios {
   public $id, $nombre, $tipo, $dificultadGlobal, $actividadId;
 
   function __construct($id, $nombre, $tipo, $dificultadGlobal, $actividadId) {
-    $this->$id = $id;
-    $this->$nombre = $nombre;
-    $this->$tipo = $tipo;
-    $this->$dificultadGlobal = $dificultadGlobal;
-    $this->$actividadId = $actividadId;
+    $this->id = $id;
+    $this->nombre = $nombre;
+    $this->tipo = $tipo;
+    $this->dificultadGlobal = $dificultadGlobal;
+    $this->actividadId = $actividadId;
   }
 
   function insert() {
     return mysql_query("insert into tablaEjercicios values " .
-		       "($this->$id, '$this->$nombre', $this->$tipo, " .
-		       "$this->$dificultadGlobal, $this->$actividadId)")
+		       "($this->id, '$this->nombre', $this->tipo, " .
+		       "$this->dificultadGlobal, $this->actividadId)")
       or mysql_error();
   }
 
   function update() {
-    return mysql_query("update tablaEjercicios set nombre = '$this->$nombre', " .
-		       "tipo = $this->$tipo, dificultadGlobal = $this->$dificultadGlobal, " .
-		       "actividadId = $this->$actividadId " .
-		       "where (id = $this->$id)")
+    return mysql_query("update tablaEjercicios set nombre = '$this->nombre', " .
+		       "tipo = $this->tipo, dificultadGlobal = $this->dificultadGlobal, " .
+		       "actividadId = $this->actividadId " .
+		       "where (id = $this->id)")
       or mysql_error();
   }
 
   function delete() {
-    return mysql_query("delete from tablaEjercicios where (id = $this->$id)")
+    return mysql_query("delete from tablaEjercicios where (id = $this->id)")
       or mysql_error();
   }
 
@@ -43,7 +43,7 @@ class TablaEjercicios {
 
   public function asignar($deportistaId) {
     return mysql_query("insert into usuario_esAsignado_tablaEjercicios values " .
-		"( $deportistaId, $this->$id )");
+		"( $deportistaId, $this->id )");
   }
 }
 ?>
