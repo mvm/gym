c1eccaf2ef6f96787c3e9c237c952bdd47e5a464
Miguel Vicente Moure <mvicmoure@gmail.com>
2017-01-05 15:17:54 +0100

    Enlace a pagina principal

--- notificacion.php
+++ notificacion.php
@@ -5,7 +5,9 @@ if(!isset($_SESSION["userId"])) {
 ?>
 <html>
 <body>
-<p><a href="?">Inbox</a> : <a href="?a=enviar">Enviar</a></p><br/>
+<h1>GYM</h1>
+<p><a href="index.php">Página principal</a></p>
+<p>Notificaciones :: <a href="?">Inbox</a> : <a href="?a=enviar">Enviar</a></p><br/>
 <div>
 <?php
 require "controllers/notificacion.php";
