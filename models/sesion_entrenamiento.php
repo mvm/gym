bd8661d2312df6b74bcca3be50b8d07b6cb4064b
Miguel Vicente Moure <mvicmoure@gmail.com>
2017-01-05 10:54:13 +0100

    Corregido sesion_entrenamiento.php

--- models/sesion_entrenamiento.php
+++ models/sesion_entrenamiento.php
@@ -3,35 +3,35 @@ class SesionEntrenamiento {
   public $id, $inicio, $fin, $nombre, $actividadId, $entrenadorId;
 
   function __construct($id, $inicio, $fin, $nombre, $actividadId, $entrenadorId) {
-    $this->$id = $id;
-    $this->$inicio = $inicio;
-    $this->$fin = $fin;
-    $this->$nombre = $nombre;
-    $this->$actividadId = $actividadId;
-    $this->$entrenadorId = $entrenadorId;
+    $this->id = $id;
+    $this->inicio = $inicio;
+    $this->fin = $fin;
+    $this->nombre = $nombre;
+    $this->actividadId = $actividadId;
+    $this->entrenadorId = $entrenadorId;
   }
 
   function insert() {
     return mysql_query("insert into sesionEntrenamiento values " .
-		       "( $this->$id, '$this->$inicio', '$this->$fin', " .
-		       "'$this->$nombre', $this->$actividadId, " .
-		       "$this->$entrenadorId)")
+		       "( $this->id, '$this->inicio', '$this->fin', " .
+		       "'$this->nombre', $this->actividadId, " .
+		       "$this->entrenadorId)")
       or mysql_error();
   }
 
   function update() {
     return mysql_query("update sesionEntrenamiento set " .
-		       "inicio = '$this->$inicio', " .
-		       "fin = '$this->$fin', " .
-		       "nombre = '$this->$nombre', " .
-		       "actividadId = $this->$actividadId, " .
-		       "entrenadorId = $this->$entrenadorId " .
-		       "where (id = $this->$id)")
+		       "inicio = '$this->inicio', " .
+		       "fin = '$this->fin', " .
+		       "nombre = '$this->nombre', " .
+		       "actividadId = $this->actividadId, " .
+		       "entrenadorId = $this->entrenadorId " .
+		       "where (id = $this->id)")
       or mysql_query();
   }
 
   function delete() {
-    return mysql_query("delete from sesionEntrenamiento where (id = $this->$id)");
+    return mysql_query("delete from sesionEntrenamiento where (id = $this->id)");
   }
 
   public static function seek($id) {
